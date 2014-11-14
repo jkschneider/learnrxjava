cf84b05077701f5892f3e50b8c1303782519c83e
Jon Schneider <joschneider@netflix.com>
2014-11-14 15:27:17 -0800

    Fix ScanVsReduceExample, fix javadocs

--- src/main/java/learnrxjava/examples/ScanVsReduceExample.java
+++ src/main/java/learnrxjava/examples/ScanVsReduceExample.java
@@ -5,18 +5,17 @@ import java.util.ArrayList;
 import rx.Observable;
 
 public class ScanVsReduceExample {
+    public static void main(String... args) {
+        Observable.range(0, 10).reduce(new ArrayList<Integer>(), (list, i) -> {
+            list.add(i);
+            return list;
+        }).forEach(System.out::println);
 
-//    public static void main(String... args) {
-//        Observable.range(0, 10).reduce(() -> new ArrayList<Integer>(), (list, i) -> {
-//            list.add(i);
-//            return list;
-//        }).forEach(System.out::println);
-//
-//        System.out.println("... vs ...");
-//
-//        Observable.range(0, 10).scan(() -> new ArrayList<Integer>(), (list, i) -> {
-//            list.add(i);
-//            return list;
-//        }).forEach(System.out::println);
-//    }
+        System.out.println("... vs ...");
+
+        Observable.range(0, 10).scan(new ArrayList<Integer>(), (list, i) -> {
+            list.add(i);
+            return list;
+        }).forEach(System.out::println);
+    }
 }
